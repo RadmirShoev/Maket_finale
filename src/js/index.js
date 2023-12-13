8663e07b9334da782f6117a826a04b111c81c8d0
Радмир <Rad_ma94@mail.ru>
2023-12-13 12:36:54 +0300

    site link

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
 import '../scss/style.scss' // Стили
 import './brends-code.js'
-import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js' //Свайпер
 import './swiper-init.js' //Инициализация Свайпера
